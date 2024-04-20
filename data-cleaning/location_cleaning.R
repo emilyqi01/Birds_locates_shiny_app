b18c00129e9233e9e760ba1615ccc8ff1e6f51d4
Yinglai Qi <yinglaiqi@Yinglais-MacBook-Pro.local>
2024-04-20 16:26:18 +0800

    do data cleaning on location dataset and update the derived dataset for further use

---
+++ data-cleaning/location_cleaning.R
@@ -0,0 +1,15 @@
+# This r script is to do data cleaning on the location csv
+# It should include deleting NAs and selecting useful variables for furthur analysis
+# Also it should save the deriving dataset
+# import libaries
+library(dplyr)
+library(tidyverse)
+# read the location dataset
+location_data <- read.csv("./data/raw_data/location.csv")
+head(location_data)
+# deal with nas
+# Select the desired columns
+selected_loc <- location_data %>%
+  select(location_id, latitude, longitude, elevation) %>% drop_na()
+  # Remove rows with NA values in any of the selected columns
+write.csv(selected_loc, "./data/derived_data/loc_update.csv",row.names = FALSE)  
