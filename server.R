11433e3b51ceedb72204126811039f2f76941981
Yinglai Qi <yinglaiqi@dyn3234-127.wlan.ic.ac.uk>
2024-05-10 19:24:12 +0100

    add more contents on analysis

--- server.R
+++ server.R
@@ -11,12 +11,39 @@ library(ggplot2)
 # Load the necessary bird data from a CSV file
 bird_data = read.csv("./data/derived_data/selected_bird.csv")
 obs_data <- read.csv("./data/derived_data/observe_update.csv")
-# Map image paths from directory to Shiny app path
-#addResourcePath("bird_images", "data/picture")
-#addResourcePath("bird_sounds", "data/audio")
 
-# Define server function to provide reactive outputs and render UI components
+# Define server function 
 server <- function(input, output) {
+  # Define a reactive expression that filters taxon frequency 
+  # based on a user-defined range from a slider input
+  filtered_taxon_freq <- reactive({
+    # Group data by taxon_id, calculate the count of occurrences for each taxon, and remove grouping structure
+    taxon_id_freq <- obs_data %>%
+      group_by(taxon_id) %>%
+      summarize(count = n(), .groups = "drop") %>%
+      arrange(desc(count)) # Sort data in descending order based on count
+    # Retrieve the range selected by the user from the slider input
+    taxon_range <- input$taxon_range_slider
+    # Filter the data to include only taxon IDs within the specified range
+    taxon_id_freq %>%
+      filter(taxon_id >= taxon_range[1] & taxon_id <= taxon_range[2])
+  })
+  
+  # Generate and display a bar plot of taxon ID frequency controlled by the slider input
+  output$taxon_id_frequency_plot <- renderPlot({
+    # Obtain the filtered data from the reactive expression
+    filtered_data <- filtered_taxon_freq()
+    # Create a bar plot using ggplot2
+    ggplot(filtered_data, aes(x = as.factor(taxon_id), y = count, fill = count)) +
+      geom_bar(stat = "identity") +
+      scale_fill_gradient(low =  "skyblue", high = "darkgreen") +
+      theme_minimal() +
+      labs(x = "Taxon ID", y = "Frequency", title = "Frequency of Taxon IDs") +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  })
+  
+  
+  
   # Reactive expression to filter data based on user selections in the UI
   filtered_data <- reactive({
     bird_data %>% 
@@ -27,17 +54,87 @@ server <- function(input, output) {
           elevation <= input$elevation_range[2]        # Filter by maximum elevation
       )
   })
+  # Read text content from about years' elevation location of birds trending
+  textContent <- readLines("texts/elevation_trend_analysis.txt", warn = FALSE)
+  trend_text <- paste("<p>", textContent, "</p>", collapse = "")
+  # Highlight specific taxa names 
+  trend_text <- gsub("Setophaga occidentalis", "<strong>Setophaga occidentalis</strong>", trend_text)
+  trend_text <- gsub("Poecile rufescens", "<strong>Poecile rufescens</strong>", trend_text)
+  trend_text <- gsub("Sitta canadensis", "<strong>Sitta canadensis</strong>", trend_text)
+  trend_text <- gsub("Troglodytes pacificus", "<strong>Troglodytes pacificus</strong>", trend_text)
+  trend_text <- gsub("Catharus ustulatus", "<strong>Catharus ustulatus</strong>", trend_text)
+  # Output the text as HTML
+  output$trend_ana <- renderUI({
+    HTML(trend_text)
+  })
+  
+  # Render a boxplot of elevation for each taxon, filtered by the selected year
+  output$elevationPlot <- renderPlot({
+    # Filter the data based on the selected year
+    data_for_plot <- bird_data %>%
+      filter(year == input$select_year)  
+    # Define a vibrant color palette
+    vibrant_palette <- RColorBrewer::brewer.pal(n = length(unique(data_for_plot$taxon_name)), name = "Set3")
+    
+    # Create the boxplot using the filtered data
+    ggplot(data_for_plot, aes(x = as.factor(taxon_name), y = elevation, color = as.factor(taxon_name), fill = as.factor(taxon_name))) +
+      geom_boxplot(alpha = 0.5, outlier.shape = 19, outlier.size = 2, lwd = 0.75) +  
+      scale_color_manual(values = vibrant_palette) +
+      scale_fill_manual(values = vibrant_palette) +
+      labs(x = "Taxon Name", y = "Elevation (m)", color = "Taxon Name", fill = "Taxon Name") +
+      theme_minimal() +
+      theme(legend.position = "bottom",
+            axis.text.x = element_text(angle = 0, hjust = 0.5),  # Set angle to 0 for horizontal labels
+            legend.text = element_text(size = 10)) +
+      guides(fill = guide_legend(override.aes = list(color = NULL)))  # Remove color border around legend keys
+  })
+  
+
+  
+  # Render a bar plot showing the count of observations per taxon for the selected year
+  output$taxon_plot <- renderPlot({
+    data <- bird_data %>%
+      filter(year == input$year_select) %>%
+      group_by(taxon_name) %>%
+      summarize(n = n(), .groups = "drop") # Group data by taxon and count occurrences
+    
+    # Wrap taxon names to avoid cluttering the x-axis
+    data$taxon_name <- str_wrap(data$taxon_name, width = 20)
+    
+    ggplot(data, aes(x = taxon_name, y = n, fill = taxon_name)) +
+      geom_col(position = "dodge") +
+      labs(title = paste("Taxon Distribution for", input$year_select), x = "Taxon Name", y = "Count") +
+      scale_fill_manual(values = c("#17648d", "#51bec7", "#d6ab63", "#843844", "#008c8f")) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
 
 
-  # Render the Leaflet map with reactive data
+  # Read text content from about years' elevation location of birds trending
+  taxoncontent <- readLines("texts/taxon_analysis.txt", warn = FALSE)
+  taxon_text <- paste("<p>", taxoncontent, "</p>", collapse = "")
+  # Highlight specific taxa names 
+  taxon_text <- gsub("Setophaga occidentalis", "<strong>Setophaga occidentalis</strong>", taxon_text)
+  taxon_text <- gsub("Poecile rufescens", "<strong>Poecile rufescens</strong>", taxon_text)
+  taxon_text <- gsub("Sitta canadensis", "<strong>Sitta canadensis</strong>", taxon_text)
+  taxon_text <- gsub("Troglodytes pacificus", "<strong>Troglodytes pacificus</strong>", taxon_text)
+  taxon_text <- gsub("Catharus ustulatus", "<strong>Catharus ustulatus</strong>", taxon_text)
+  # Output the text as HTML
+  output$taxon_ana <- renderUI({
+    HTML(taxon_text)
+  })
+
+  
+  
+  # Render a Leaflet map to display the distribution of taxa with interactive features
   output$map <- renderLeaflet({
-    # Define color palette within reactive context
+    # Define a color palette for the map based on taxon names
     pal <- colorFactor(
       palette = c("#17648d", "#51bec7", "#d6ab63", "#843844", "#008c8f"),
-      domain = bird_data$taxon_name  # Use static dataset for domain
+      domain = bird_data$taxon_name  
     )
     
-    leaflet(data = filtered_data()) %>%
+    leaflet(data = filtered_data()) %>% 
       addTiles() %>%
       addCircleMarkers(
         ~longitude, ~latitude,
@@ -52,63 +149,7 @@ server <- function(input, output) {
                        "' style='width:100px; height:auto;'>")
       )
   })
-
   
-  # Render a bar plot showing the count of observations per taxon for the selected year
-  output$taxon_plot <- renderPlot({
-    data <- bird_data %>%
-      filter(year == input$year_select) %>%
-      group_by(taxon_name) %>%
-      summarize(n = n(), .groups = "drop")
-    
-    # Wrap taxon names to avoid cluttering the x-axis
-    data$taxon_name <- str_wrap(data$taxon_name, width = 20)
-    
-    ggplot(data, aes(x = taxon_name, y = n, fill = taxon_name)) +
-      geom_col(position = "dodge") +
-      labs(title = paste("Taxon Distribution for", input$year_select), x = "Taxon Name", y = "Count") +
-      scale_fill_manual(values = c("#17648d", "#51bec7", "#d6ab63", "#843844", "#008c8f")) +
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  })
-
-  
-  # Function to create a non-interactive plot based on taxon_id frequency
-  # Reactive expression for filtered taxon data based on selected slider range
-  filtered_taxon_freq <- reactive({
-    taxon_id_freq <- obs_data %>%
-      group_by(taxon_id) %>%
-      summarize(count = n(), .groups = "drop") %>%
-      arrange(desc(count))
-    
-    taxon_range <- input$taxon_range_slider
-    taxon_id_freq %>%
-      filter(taxon_id >= taxon_range[1] & taxon_id <= taxon_range[2])
-  })
-  
-  # Update taxon frequency plot with slider range selection
-  output$taxon_id_frequency_plot <- renderPlot({
-    filtered_data <- filtered_taxon_freq()
-    ggplot(filtered_data, aes(x = as.factor(taxon_id), y = count, fill = count)) +
-      geom_bar(stat = "identity") +
-      scale_fill_gradient(low =  "skyblue", high = "darkgreen") +
-      theme_minimal() +
-      labs(x = "Taxon ID", y = "Frequency", title = "Frequency of Taxon IDs") +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  })
-  
-  output$elevationPlot <- renderPlot({
-    # Filter the data based on the selected year
-    data_for_plot <- bird_data %>%
-      filter(year == input$select_year)  # Make sure to use the correct inputId for year selection
-    
-    # Create the plot
-    ggplot(data_for_plot, aes(x = as.factor(taxon_id), y = elevation, color = as.factor(taxon_id))) +
-      geom_boxplot() +
-      labs(x = "Taxon ID", y = "Elevation (m)", color = "Taxon ID") +
-      theme_minimal() +
-      theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, hjust = 1))
-  })
   
   
   
--- ui.R
+++ ui.R
@@ -7,10 +7,12 @@ library(ggplot2)
 # Load the data
 bird_data = read.csv("./data/derived_data/selected_bird.csv")
 obs_data <- read.csv("./data/derived_data/observe_update.csv")
+# Create the shiny dashboard header
 header <- dashboardHeader(
   title = HTML("&#x1F332; Forest Bird Observations &#x1F426;"),
-  titleWidth = 300 # Adjust this based on your dashboard's layout
+  titleWidth = 300 
 )
+
 # Define the sidebar content with filters
 sidebar = dashboardSidebar(
   width = 300,
@@ -22,6 +24,7 @@ sidebar = dashboardSidebar(
     menuItem("Elevation_selection", tabName = "ele", icon = icon("filter")),
     menuItem("Analyzing", tabName = "analysis", icon = icon("chart-line"))
   ),
+  # only appear when we want to visualize on maps
   conditionalPanel(
     condition = "input.sidebar === 'maping'",
     div(style = "padding: 20px;",
@@ -56,11 +59,10 @@ body = dashboardBody(
     tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
   ),
   
-  
     tabItem(tabName = "instructions",
             # Display this panel if either the instructions or filters tab is active
             conditionalPanel(
-              condition = "input.sidebar == 'instructions' || input.sidebar == 'maping'",
+              condition = "input.sidebar == 'instructions'",
               fluidRow(
                 box(
                   width = 12,
@@ -118,28 +120,68 @@ body = dashboardBody(
               )
             )
     ),
-    
+  tabItem(tabName = "maping",
+          
+          conditionalPanel(
+            condition = "input.sidebar == 'maping'",
+            fluidRow(
+              box(
+                width = 12,
+                title = "Let's Play to visualize on the map!!",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                collapsed = FALSE,
+                status = "primary", 
+                p("This is a dashboard for bird observation in specific areas from 2009 to present. 
+                    Users could use it to discover birds' locations (including latitude, altitude and elevation)over years."),
+                p("Use the controls to filter the bird observation data based on year, taxon ID, and elevation range. 
+                    The map will update automatically to reflect the filters applied."),
+                p("Hover over points on the map to see more details about each observation."),
+                
+              ),
+              box(width = 12, leafletOutput("map", height = "500px")),
+              box(width = 12, title = "Analysis of Taxon Distribution Across Multiple Years",
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  status = "primary",
+                  htmlOutput("taxon_ana")
+                  )
+            )
+            
+          )
+  ),
     tabItem(tabName = "ele",
             conditionalPanel(
               condition = "input.sidebar == 'ele'",
-              fluidRow(selectInput(
-                inputId = "select_year",
-                label = "Select Year:",
-                choices = unique(bird_data$year),
-                selected = unique(bird_data$year)[1]
-              )),
+              fluidRow(
+                column(6,  # Adjust width as needed for the text
+                       h4("Select your interested year:"),  # Example text element
+                       p("Please select the year you want to examine. The data will be filtered accordingly.")
+                ),
+                column(6,  # Adjust the remaining space for the selectInput
+                       selectInput(
+                         inputId = "select_year",
+                         label = "Select Year:",
+                         choices = unique(bird_data$year),
+                         selected = unique(bird_data$year)[1]
+                       )
+                ),
               box(
                 title = "Taxon ID Frequency",
                 width = 12,
                 plotOutput("elevationPlot", height = "300px")
-              )
-            )
-    ),
-    tabItem(tabName = "maping",
-            conditionalPanel(
-              condition = "input.sidebar == 'maping'",
-              box(width = 12, leafletOutput("map", height = "500px"))
-            )
+              ),
+              box(
+                width = 12,
+                title = "Bird Observation Analysis at H.J. Andrews Experimental Forest",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                collapsed = FALSE,
+                status = "primary",
+                htmlOutput("trend_ana"))
+              )),
+              
     ),
   tabItem(tabName = "analysis",
           # Display this panel if either the instructions or filters tab is active
@@ -177,31 +219,46 @@ body = dashboardBody(
                     style = "display: flex; align-items: center; gap: 40px;", 
                     img(src = "picture/Setophaga_occidentalis.jpeg", width = 120, height = 90),
                     tags$audio(src = "audio/Setophaga_occidentalis.mp3", type = "audio/mp3", controls = TRUE),
-                    tags$span("Description for Catharus ustulatus")
+                    tags$span("In the sun-dappled branches of the western forests, the Setophaga occidentalis, 
+                    or the Hermit Warbler, shares its musical gift with the world. Its song is a delicate tapestry of high notes, each one pure and clear, 
+                              carrying a sweetness that transcends the buzzing quality found in the song of the Townsend's Warbler.")
                   ),
                   tags$li(
                     style = "display: flex; align-items: center; gap: 40px;", 
                     img(src = "picture/Poecile_rufescens.jpeg", width = 120, height = 90),
                     tags$audio(src = "audio/Poecile_rufescens.mp3", type = "audio/mp3", controls = TRUE),
-                    tags$span("Description for Catharus ustulatus")
+                    tags$span("The Poecile rufescens, affectionately known as the chestnut-backed Chickadee, fills the forest with its lively chatter, 
+                    a squeaky chick-a-dee that dances through the air, sharper and swifter than the calls of its avian kin. It's a creature of whimsy, 
+                    often punctuating the silence with its thin, delicate tsee-deee, a sound that seems to shimmer amidst the leaves, 
+                              accompanied by delicate lisping notes that flutter like whispers on the wind. ")
                   ),
                   tags$li(
                     style = "display: flex; align-items: center; gap: 40px;", 
                     img(src = "picture/Troglodytes_pacificus.jpeg", width = 120, height = 90),
                     tags$audio(src = "audio/Troglodytes_pacificus.mp3", type = "audio/mp3", controls = TRUE),
-                    tags$span("Description for Catharus ustulatus")
+                    tags$span("In the dense undergrowth, the Troglodytes pacificus, known as the Pacific Wren, 
+                    orchestrates its symphony of sound, a cacophony of high-pitched, 
+                    diverse trills and chatters that cascade through the air with breathtaking speed and variation.
+                              Its call, an explosive burst of energy, echoes through the woodland, 
+                              a sharp \"kit!\" or \"kit-kit!\" that reverberates amidst the lush greenery, 
+                              marking its presence in the wilderness with fervent enthusiasm.")
                   ),
                   tags$li(
                     style = "display: flex; align-items: center; gap: 40px;", 
                     img(src = "picture/Sitta_canadensis.jpeg", width = 120, height = 90),
                     tags$audio(src = "audio/Sitta_canadensis.mp3", type = "audio/mp3", controls = TRUE),
-                    tags$span("Description for Catharus ustulatus")
+                    tags$span("In the tranquil woodland, the Sitta canadensis, or the Red-breasted Nuthatch, 
+                    adds its unique melody to the natural chorus, a tinny \"yank-yank\" that pierces the air with its distinctiveness, 
+                              rising higher in pitch and imbued with a nasal quality unlike the call of the White-breasted Nuthatch.")
                   ),
                   tags$li(
                     style = "display: flex; align-items: center; gap: 40px;", 
                     img(src = "picture/Catharus_ustulatus.jpeg", width = 120, height = 90),
                     tags$audio(src = "audio/Catharus_ustulatus.mp3", type = "audio/mp3", controls = TRUE),
-                    tags$span("Description for Catharus ustulatus")
+                    tags$span("In the hushed canopy of the forest, the Catharus ustulatus,
+                    known as the Swainson's Thrush, unveils its melodic masterpiece, a song that weaves through the air like delicate threads of sound. Its ethereal melody unfolds in a series of reedy spiraling notes, each inflected upward, 
+                              creating a mesmerizing symphony that resonates through the woodland, 
+                              captivating all who are fortunate enough to hear it.")
                   )
                 )
               )
